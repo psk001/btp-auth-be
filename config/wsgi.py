ab40af5b18682af3b935748aeaa5102a7ae0adf1
pushpak <pushpak9208@gmail.com>
2023-06-19 17:52:32 +0530

    fixed

--- config/wsgi.py
+++ config/wsgi.py
@@ -17,6 +17,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 app = get_wsgi_application()
 
